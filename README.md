18f13f782562862fe606404b8a9e9cef25de6338
Chenran Xu <chenran.xu@kit.edu>
2022-11-14 18:20:30 +0100

    Move BO logic into a class

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # Bayesian Optimization attempts for European XFEL tuning
 
-Basic Idea: It reads the configuration from a dict or a json / yaml file and starts the Bayesian optimization.
+__Basic Idea__: It reads the configuration from a dict or a json / yaml file and starts the Bayesian optimization.
 
 ## Project Structure
 
 - `simplebo.py` Implements the basic BO logic
+
+## Usage
+
+### Installing Requirements
+
+It is recommended to use a virtual environment, e.g. conda. With the environment activated, simply do
+
+```bash
+pip install -r requirements.txt
+```
+
+### Using BO
+
+---
 
 ## Tasks
 
@@ -21,4 +35,4 @@ TODO:
   - [ ] BO test with simple mathematical functions
   - [ ] I/O test with pydoocs
   - [ ] (Check compatibility) Environment test on control room PCs
-- [ ] (If enough time): try direct interface with [Xopt](https://github.com/ChristopherMayes/Xopt)
+- [ ] (If enough time): try direct interface with [Xopt](https://github.com/ChristopherMayes/Xopt), this will be a sustainable and more preferred way for productive tools.
--- simplebo.py
+++ simplebo.py
@@ -1,12 +1,117 @@
-import botorch
-import gpytorch
+import time
+from typing import Callable, Optional, Union
+
 import numpy as np
 import torch
-import time
-from typing import Optional, Union
+from botorch.acquisition import (
+    ExpectedImprovement,
+    ProbabilityOfImprovement,
+    UpperConfidenceBound,
+)
+from botorch.models import SingleTaskGP
+from botorch.models.transforms import Standardize
+from botorch.models.transforms.outcome import OutcomeTransform
+from botorch.optim import optimize_acqf
+from gpytorch.mlls import ExactMarginalLogLikelihood
 
 
-def simple_bayesopt(problem_config: dict, readonly=False, **kwargs) -> dict:
+class SimpleBO:
+    def __init__(
+        self,
+        problem_config: dict,
+        readonly=False,
+        active_params: Optional[list(int)] = None,
+        acquisition: str = "EI",
+        outcome_transofrm: Optional[OutcomeTransform] = Standardize,
+        **kwargs
+    ) -> None:
+        self.read_config(problem_config, active_params)
+        self.acquisition = acquisition
+        self.readonly = readonly
+
+        self.outcome_transform = outcome_transofrm
+        # Initialize Logging
+        self.steps_taken = 0
+
+        pass
+
+    def read_config(self, config_file: dict, active_param: Optional[list] = None):
+
+        if active_param is None:  # use all channels listed
+            active_param = list(range(config_file["id"]))
+        self.input_params = config_file["id"][active_param]
+        self.n_params = len(self.input_params)
+        self.objective_func = config_file["fun_a"]
+        self.max_steps = config_file["max_iter"]
+        self.bounds = torch.tensor(config_file["lims"])[active_param]
+
+        pass
+
+    def optimize(self, callback: Optional[Callable] = None):
+
+        # Optimization Loop
+        while self.steps_taken < self.max_steps:
+            self.step()
+            if callback is not None:  # Do something between steps?
+                callback
+
+    def init_bo(self):
+        # initial design of the BO, ...
+        pass
+
+    def step(self):
+        """Take one BO step"""
+
+        x_next = self.suggest_next_sample()
+
+        y = self.evaluate_objective(x_next.detach().numpy())
+
+        # Log history
+        self.steps_taken += 1  # increase step count
+        # Append data
+        self.X = torch.cat([self.X, x_next])
+        self.Y = torch.cat([self.Y, y])
+        pass
+
+    def suggest_next_sample(self) -> torch.Tensor:
+
+        # Construct and fit GP
+        self.gp = SingleTaskGP(self.X, self.Y, outcome_transform=self.outcome_transform)
+        mll = ExactMarginalLogLikelihood(self.gp.likelihood, self.gp)
+
+        # Build acquisition function
+        self.acqf = self._build_acqf()
+        # Maximize acquisition
+        candidates, _ = optimize_acqf(
+            acq_function=self.acqf,
+            bounds=self.bounds,
+            q=1,
+            num_restarts=10,
+            raw_samples=128,
+            options={"maxiter": 200},
+        )
+
+        # Return parameter setting for next evaluation
+        return candidates
+
+    def _build_acqf(self):
+        if self.acquisition == "EI":
+            self.acqf = ExpectedImprovement(self.gp, self.Y.max())
+        elif self.acquisition == "UCB":
+            self.acqf = UpperConfidenceBound(self.gp, beta=0.2)
+        elif self.acquisition == "PI":
+            self.acqf = ProbabilityOfImprovement(self.gp, self.Y.max())
+
+    def evaluate_objective(self, input) -> torch.Tensor:
+        # Set new parameters
+        if not self.readonly:
+            _set_new_parameters(input, param_names=self.input_params)
+        else:
+            print("Testing: will skip setting parameters...")
+        # Get objective function
+        objective = _get_objective(obj_func=self.objective_func)
+        return torch.Tensor(objective)
+
     """A simple Bayesian optimization routine
 
     Parameters
@@ -27,50 +132,6 @@ def simple_bayesopt(problem_config: dict, readonly=False, **kwargs) -> dict:
 
 
     """
-    log = {}
-    max_steps = problem_config["max_iter"]
-    if kwargs.has_key("active_param"):  # use selected channels as input
-        active_param = kwargs["active_param"]
-    else:  # use all channels listed
-        active_param = list(range(problem_config["id"]))
-    input_params = problem_config["id"][active_param]
-    n_params = len(input_params)
-    objective_func = problem_config["fun_a"]
-
-    # Initialize the BO
-    X = torch.zeros([1, n_params])
-    Y = torch.zeros([1, 1])
-
-    # BO Loop
-    for i in range(max_steps):
-        # Get Next Parameter Setting
-        new_action = suggest_next_sample()
-        new_action = new_action.detach().numpy()  # convert to numpy
-
-        # Evaluate Function
-        y = evaluate_objective(
-            new_action, objective_func=objective_func, readonly=readonly
-        )
-
-        # Append data
-
-        # Log the optimization step
-
-    # Optional, set back to best value
-
-    return log
-
-
-def suggest_next_sample() -> torch.Tensor:
-
-    # Construct and Fit GP
-
-    # Build Acquisition
-
-    # Maximize Acquisition
-
-    # Return parameter setting for next evaluation
-    pass
 
 
 def _set_new_parameters(
@@ -93,20 +154,3 @@ def _get_objective(
     if minimize:  # invert the objective if minimization
         averaged_obj *= -1
     return averaged_obj
-
-
-def evaluate_objective(
-    input: np.ndarray,
-    param_names: Optional[list[str]] = None,
-    objective_func: str = None,
-    readonly: bool = False,
-    **kwargs
-) -> torch.Tensor:
-    # Set new parameters
-    if not readonly:
-        _set_new_parameters(input, param_names=param_names)
-    else:
-        print("Testing: will skip setting parameters...")
-    # Get objective function
-    objective = _get_objective(obj_func=objective_func)
-    return torch.Tensor(objective)
