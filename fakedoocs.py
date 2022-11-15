ff2985d73f8e678f0e307019b620608e8a5be9a5
Chenran Xu <chenran.xu@kit.edu>
2022-11-15 14:36:27 +0100

    Finish bo logic, add fakedoocs for testing

---
+++ fakedoocs.py
@@ -0,0 +1,12 @@
+"""A dummy module providing pyDOOCS similar interface for testing purpose"""
+
+
+def read(address):
+    pass
+
+
+def write(address, value):
+    pass
+
+
+# Test functions
--- simplebo.py
+++ simplebo.py
@@ -1,3 +1,4 @@
+import json
 import time
 from typing import Callable, Optional, Union
 
@@ -8,31 +9,43 @@ from botorch.acquisition import (
     ProbabilityOfImprovement,
     UpperConfidenceBound,
 )
+from botorch.fit import fit_gpytorch_model
 from botorch.models import SingleTaskGP
-from botorch.models.transforms import Standardize
+from botorch.models.transforms import Normalize, Standardize
+from botorch.models.transforms.input import InputTransform
 from botorch.models.transforms.outcome import OutcomeTransform
 from botorch.optim import optimize_acqf
 from gpytorch.mlls import ExactMarginalLogLikelihood
+
+import fakedoocs as pydoocs
+
+# import pydoocs
 
 
 class SimpleBO:
     def __init__(
         self,
         problem_config: dict,
-        readonly=False,
+        readonly: bool = False,
         active_params: Optional[list(int)] = None,
         acquisition: str = "EI",
         outcome_transofrm: Optional[OutcomeTransform] = Standardize,
-        **kwargs
+        input_transform: Optional[InputTransform] = Normalize,
+        step_limit_type: str = "proximal",
+        step_size: int = 0.1,
     ) -> None:
         self.read_config(problem_config, active_params)
         self.acquisition = acquisition
         self.readonly = readonly
 
-        self.outcome_transform = outcome_transofrm
-        # Initialize Logging
-        self.steps_taken = 0
+        self.outcome_transform = outcome_transofrm(1)  # standardize y
+        self.input_transform = input_transform(self.n_params)  # normalize x
 
+        self.step_size = step_size
+        self.step_limit_type = step_limit_type
+
+        # Initialize Logging
+        self.reset()
         pass
 
     def read_config(self, config_file: dict, active_param: Optional[list] = None):
@@ -43,21 +56,44 @@ class SimpleBO:
         self.n_params = len(self.input_params)
         self.objective_func = config_file["fun_a"]
         self.max_steps = config_file["max_iter"]
+        self.nreadings = config_file["nreadings"]
+        self.interval = config_file["interval"]
         self.bounds = torch.tensor(config_file["lims"])[active_param]
-
+        self.maximize = config_file["maximization"]
         pass
 
     def optimize(self, callback: Optional[Callable] = None):
+        if not self.initialized:
+            self.init_bo()
 
         # Optimization Loop
         while self.steps_taken < self.max_steps:
             self.step()
             if callback is not None:  # Do something between steps?
-                callback
+                callback()
 
-    def init_bo(self):
-        # initial design of the BO, ...
-        pass
+    def reset(self):
+        """Resets to initial state, clean history, similar idea as a gym `env.reset()`"""
+        self.history = {"steps:": []}
+        self.X = None
+        self.Y = None
+        self.steps_taken = 0
+        self.initialized = False
+
+    def init_bo(self, n_init: int = 5):
+        # initial design of the BO, sample random initial points
+        if (self.X is not None) or (self.Y is not None):
+            print("Warning: BO already initialized before, reinitializing...")
+
+        self.X = torch.rand((self.n_params, n_init), dtype=torch.double)
+        self.Y = torch.zeros(n_init)
+
+        # Sample initial settings
+        for i, x in enumerate(self.X):
+            y = self.evaluate_objective(x.detach().numpy())
+            self.Y[i] = y
+
+        self.initialized = True
 
     def step(self):
         """Take one BO step"""
@@ -74,13 +110,20 @@ class SimpleBO:
         pass
 
     def suggest_next_sample(self) -> torch.Tensor:
-
+        """Core BO step, suggest next candidate setting"""
         # Construct and fit GP
-        self.gp = SingleTaskGP(self.X, self.Y, outcome_transform=self.outcome_transform)
+        self.gp = SingleTaskGP(
+            self.X,
+            self.Y,
+            outcome_transform=self.outcome_transform,
+            input_transform=self.input_transform,
+        )
         mll = ExactMarginalLogLikelihood(self.gp.likelihood, self.gp)
-
+        fit_gpytorch_model(mll)
         # Build acquisition function
         self.acqf = self._build_acqf()
+        # Calculate new bounds if hard stepsize limit
+
         # Maximize acquisition
         candidates, _ = optimize_acqf(
             acq_function=self.acqf,
@@ -101,6 +144,10 @@ class SimpleBO:
             self.acqf = UpperConfidenceBound(self.gp, beta=0.2)
         elif self.acquisition == "PI":
             self.acqf = ProbabilityOfImprovement(self.gp, self.Y.max())
+        if self.step_limit_type == "proximal":
+            # Apply proximal biasing
+            print("Proximal biasing to be implemented...")
+            pass
 
     def evaluate_objective(self, input) -> torch.Tensor:
         # Set new parameters
@@ -109,8 +156,18 @@ class SimpleBO:
         else:
             print("Testing: will skip setting parameters...")
         # Get objective function
-        objective = _get_objective(obj_func=self.objective_func)
-        return torch.Tensor(objective)
+        objective = _get_objective(
+            obj_func=self.objective_func,
+            nreadings=self.nreadings,
+            interval=self.interval,
+            maximize=self.maximize,
+        )
+        return torch.Tensor(objective, dtype=torch.double)
+
+    def save(self, filename: str = "log/defaultlog.json"):
+        with open(filename, "w") as f:
+            json.dump(self.history, f, indent=4)
+        pass
 
     """A simple Bayesian optimization routine
 
@@ -138,19 +195,26 @@ def _set_new_parameters(
     input: np.ndarray, param_names: Optional[list[str]] = None, **kwargs
 ):
     # set new parameters via pydoocs
-    pass
+    assert len(input) == len(param_names)
+
+    for i, param in enumerate(param_names):
+        pydoocs.write(param, input[i])
+
+    # wait until settle?
 
 
 def _get_objective(
-    obj_func: str, nreadings: int = 1, interval: float = 0.1, minimize=False
+    obj_func: str, nreadings: int = 1, interval: float = 0.1, maximize: bool = True
 ) -> Union[float, np.ndarray]:
 
     assert nreadings > 0
-    objs = None
+    objs = []
     for _ in range(nreadings):
-        objs.append()
+        objs.append(pydoocs.read(obj_func))
         time.sleep(interval)  # old fashioned way :)
+
     averaged_obj = np.mean(objs)
-    if minimize:  # invert the objective if minimization
+    if not maximize:  # invert the objective if minimization
         averaged_obj *= -1
+
     return averaged_obj
